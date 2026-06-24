cb4d393429ccde1cb2ca7734149c93af93cb59f4
Jothika <raviravi31799@gmail.com>
2026-06-24 17:31:11 +0530

    first commit

---
+++ tests/demo.test.ts
@@ -0,0 +1,8 @@
+import {test,expect} from '@playwright/test';
+test('Login Test',async({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/login');
+    await page.fill('#username','tomsmith');
+    await page.fill('#password','SuperSecretPassword!');
+    await page.click('button[type="submit"]');
+    await expect(page.locator('.flash.success')).toBeVisible();
+});
